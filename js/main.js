abdbf9fade177b4137cbfdf3c1f1c0520ebe3ae2
Kadirov169 <kadirovdev169@gmail.com>
2022-02-13 20:25:15 +0500

    finished benefits section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const burger = document.querySelector(".header__burger");
 const nav = document.querySelector(".header__nav");
 const link = document.querySelector(".header__link");
-const button = document.querySelectorAll(".faqs__item-button");
-const text = document.querySelectorAll(".faqs__item-toggle-text");
+const button = document.querySelector(".faqs__item-button-active");
+const text = document.querySelector(".faqs__item-toggle-text-active");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("header__burger--opened");
   nav.classList.toggle("header__nav--opened");
   link.classList.toggle("header__link--opened");
+})
+
+button.addEventListener("click", () => {
+  text.classList.toggle("faqs__item--opened")
 })
 
  
