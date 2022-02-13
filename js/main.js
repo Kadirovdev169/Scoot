593538e3d89da9db668dc95cdc51fe713608b235
Kadirov169 <kadirovdev169@gmail.com>
2022-02-13 18:51:28 +0500

    finished home page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 const burger = document.querySelector(".header__burger");
 const nav = document.querySelector(".header__nav");
-const link = document.querySelector(".header__link")
+const link = document.querySelector(".header__link");
+const button = document.querySelectorAll(".faqs__item-button");
+const text = document.querySelectorAll(".faqs__item-toggle-text");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("header__burger--opened");
   nav.classList.toggle("header__nav--opened");
   link.classList.toggle("header__link--opened");
 })
+
