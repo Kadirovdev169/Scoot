04c0dbb4afb0f05db020d590f220ce2febc282aa
Kadirov169 <kadirovdev169@gmail.com>
2022-02-15 12:06:35 +0500

    repair trouble

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".header__burger");
 const nav = document.querySelector(".header__nav");
 const link = document.querySelector(".header__link");
 const button = document.querySelector(".faqs__item-button-active");
+const item = document.querySelector(".faqs__list-item-active")
 const text = document.querySelector(".faqs__item-toggle-text-active");
 
 burger.addEventListener("click", () => {
@@ -10,8 +11,11 @@ burger.addEventListener("click", () => {
   link.classList.toggle("header__link--opened");
 })
 
+item.addEventListener("click", () => {
+  text.classList.toggle("faqs__item--opened")
+})
+
 button.addEventListener("click", () => {
   text.classList.toggle("faqs__item--opened")
 })
 
- 
